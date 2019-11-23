e1451d7ab4ef8be91cb30ecd9fed40fcb12c3e85
KID <50666207+KID421@users.noreply.github.com>
2019-11-23 16:59:48 +0800

    水管碰撞器設定

--- UnityProject/Assets/Scripts/Bird.cs
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -4,16 +4,30 @@ public class Bird : MonoBehaviour
 {
     [Header("跳躍高度"), Range(10, 2000)]
     public int jump = 100;
+    [Header("旋轉角度"), Range(0, 100)]
+    public float angle = 10;
     [Header("是否死亡"), Tooltip("用來判斷角色是否死亡，true 死亡，false 還沒死亡")]
     public bool dead;
     [Header("剛體")]
     public Rigidbody2D r2d;
+
+    public GameObject goScore, goGM;
 
     /// <summary>
     /// 小雞跳躍方法。
     /// </summary>
     private void Jump()
     {
+        // 原本寫法
+        //if (dead == true)
+        //{
+        //    return;
+        //}
+
+        // 如果 死亡 跳出此程式區塊
+        // 判斷式只有一行敘述可以省略大括號
+        if (dead) return;   // 簡寫
+
         // 如果 玩家 按下 左鍵
         // 輸入.按下按鍵(按鍵列舉.滑鼠左鍵) (手機觸控)
         if (Input.GetKeyDown(KeyCode.Mouse0))
@@ -22,7 +36,12 @@ public class Bird : MonoBehaviour
             r2d.Sleep();                        // 小雞剛體.睡著() - 重設剛體所有資訊
             r2d.gravityScale = 1;               // 小雞剛體.重力 = 1;
             r2d.AddForce(new Vector2(0, jump)); // 小雞剛體.增加推力(二維向量(左右，上下));
+            goScore.SetActive(true);            // 分數 顯示
+            goGM.SetActive(true);               // GM 顯示
         }
+        // Rigidbody2D.SetRotation(float) 設定角度(角度)
+        // Rigidbody2D.velocity 加速度 (二維向量 x, y)
+        r2d.SetRotation(angle * r2d.velocity.y);
     }
 
     /// <summary>
@@ -30,11 +49,25 @@ public class Bird : MonoBehaviour
     /// </summary>
     private void Dead()
     {
-
+        print("死亡!!!");
+        dead = true;
     }
 
-    private void Update()
+    // 固定幀數 0.002 一幀：要控制物理請寫在此事件內
+    private void FixedUpdate()
     {
         Jump();
+    }
+
+    // 事件：碰撞開始 - 碰撞開始時執行一次 (Collision2D hit) (碰撞類別 名稱) 存放碰到物件的資訊
+    private void OnCollisionEnter2D(Collision2D hit)
+    {
+        // 碰到物件.遊戲物件.名稱
+        print(hit.gameObject.name);
+
+        if (hit.gameObject.name == "地板")
+        {
+            Dead();
+        }
     }
 }
